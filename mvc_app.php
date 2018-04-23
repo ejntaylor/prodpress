d17c310e4a620cad1a1ad8ea3f3924cfd739e86b
Elliot <elliot@raison.co>
2018-04-23 15:14:19 +0100

    Remove locate_template (obsolete)

--- mvc_app.php
+++ mvc_app.php
@@ -196,49 +196,6 @@ add_shortcode( 'mvc_app', 'mvc_app' );
 
 
 
-// locate template
-
-
-/**
- * Locate template.
- *
- * Locate the called template.
- * Search Order:
- * 1. /themes/theme/woocommerce-plugin-templates/$template_name
- * 2. /themes/theme/$template_name
- * 3. /plugins/woocommerce-plugin-templates/templates/$template_name.
- *
- * @since 1.0.0
- *
- * @param 	string 	$template_name			Template to load.
- * @param 	string 	$string $template_path	Path to templates.
- * @param 	string	$default_path			Default path to template files.
- * @return 	string 							Path to the template file.
- */
-function wcpt_locate_template( $template_name, $template_path = '', $default_path = '' ) {
-	// Set variable to search in woocommerce-plugin-templates folder of theme.
-	if ( ! $template_path ) :
-		$template_path = 'woocommerce-plugin-templates/';
-	endif;
-	// Set default plugin templates path.
-	if ( ! $default_path ) :
-		$default_path = plugin_dir_path( __FILE__ ) . 'templates/'; // Path to the template folder
-	endif;
-	// Search template file in theme folder.
-	$template = locate_template( array(
-		$template_path . $template_name,
-		$template_name
-	) );
-	// Get plugins template file.
-	if ( ! $template ) :
-		$template = $default_path . $template_name;
-	endif;
-	return apply_filters( 'wcpt_locate_template', $template, $template_name, $template_path, $default_path );
-}
-
-
-
-
 // auto setup url
 
 add_action('init', function() {
